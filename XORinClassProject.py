cef2f41a9c399f70ab574f1ff26a5acbf465fd7b
emmett reibel <emmett.reibel@gmail.com>
2024-02-27 21:16:29 +0000

    it works!

--- XORinClassProject.py
+++ XORinClassProject.py
@@ -17,3 +17,39 @@ def decode(binary):
     charIndex = int(binary, 2)
     return characters[charIndex]
 
+def XOR (bit1, bit2):
+    if bit1 == bit2:
+        return "0"
+    else:
+        return "1"
+
+def XORonByte (byte, key):
+    encryptedByte = ""
+    for bit in range(len(byte)):
+        encryptedByte += (XOR(byte[bit], key[bit]))
+    return encryptedByte
+
+
+def XORonLetter (letter, keyLetter):
+    binaryL = encode(letter)
+    binaryK = encode(keyLetter)
+    
+    encryptedLetter = XORonByte(binaryL, binaryK)
+    return decode(encryptedLetter)
+
+def XORonSentence (sentence, key):
+    superKey = ""
+    superKey += key
+
+    while len(superKey) < len(sentence):
+        superKey += key
+    
+    encryptedSentence = ""
+    
+    for i in range(len(sentence)):
+        encryptedSentence += XORonLetter(sentence[i], superKey[i])
+    return encryptedSentence
+
+print(XORonSentence("&avrvLYpjgiWtmewbSfq bl", "Beaver believers, leave"))
+print(XORonSentence("HELP ME", "hi"))
+print(XORonSentence("MwIHZUz", "hi"))
